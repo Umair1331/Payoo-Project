e1c2a02ed993eb6d851c01ee152a73e295a105d6
DewanUmair <dewanumair95@gmail.com>
2026-03-29 19:03:40 +0600

    complete Add Money Section

--- scripts/addMoney.js
+++ scripts/addMoney.js
@@ -1,3 +1,28 @@
-document.getElementById("add-money-btn").addEventListener("click", function(){
-    console.log('add money')
+document.getElementById("add-money-btn").addEventListener("click", function () {
+    // 1-- Bank Account Get
+    const bankAccount = getValueFromInput("add-money-bank");
+    if (bankAccount == 'Select a Bank') {
+        alert('Please select a bank');
+        return;
+    }
+    // 2-- Get bank account number
+    const accno = getValueFromInput("add-money-number");
+    if (accno.length != 11) {
+        alert('invalid acc no');
+        return;
+    }
+    // 3-- get amount
+    const amount = getValueFromInput("add-money-amount");
+    const currentBalance = getBalance();
+    const newBalance = currentBalance + Number(amount);
+    const pin = getValueFromInput("add-money-pin")
+    if (pin == '1234') {
+        alert(`Add Money Success from
+             ${bankAccount} at ${new Date}`);
+        setBalance(newBalance);
+    } else {
+        alert('Invalid Pin');
+        return;
+    }
+
 })
